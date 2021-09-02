6ab302f482a19487e1043221e200b45877687779
Rafael Cavalheiro <rafael.g.cavalheiro@gmail.com>
2021-09-02 07:06:19 -0300

    Add files via upload

---
+++ firsttest.py
@@ -0,0 +1,26 @@
+import json
+import os
+import openai
+
+openai.api_key = 'api_key'
+
+
+
+stop = ""
+while(stop!="exit"):
+  stop = input("Human: ")
+  response = openai.Completion.create(
+    engine="davinci",
+    prompt="\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman:" + stop + "\n AI:",
+    temperature=0.9,
+    max_tokens=150,
+    top_p=1,
+    frequency_penalty=0.0,
+    presence_penalty=0.6,
+    stop=["\n", " Human:", " AI:"]
+  )
+  object_string = repr(response.choices)
+  object_string = json.JSONDecoder(object_string)
+  print(object_string)
+
+
